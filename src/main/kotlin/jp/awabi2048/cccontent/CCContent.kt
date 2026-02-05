b6a568d96cb1ac05cc797bf98ed75efe9e344c1c
awabi2048 <awabi4096@gmail.com>
2026-02-05 23:41:24 +0900

    Phase 1: カスタムアイテム統一管理システムの実装
    - CustomItem インターフェースの実装
    - CustomItemManager による一元管理
    - /cc give コマンドの実装
    - メインプラグインクラスの拡張
    - 各フィーチャーディレクトリ構成の作成

--- src/main/kotlin/jp/awabi2048/cccontent/CCContent.kt
+++ src/main/kotlin/jp/awabi2048/cccontent/CCContent.kt
@@ -1,10 +1,30 @@
 package jp.awabi2048.cccontent
 
+import jp.awabi2048.cccontent.command.CCCommand
+import jp.awabi2048.cccontent.command.GiveCommand
+import jp.awabi2048.cccontent.items.CustomItemManager
 import org.bukkit.plugin.java.JavaPlugin
 
 class CCContent : JavaPlugin() {
     
+    companion object {
+        lateinit var instance: CCContent
+            private set
+    }
+    
     override fun onEnable() {
+        instance = this
+        
+        // ロガーをCustomItemManagerに設定
+        CustomItemManager.setLogger(logger)
+        
+        // コマンド登録
+        val giveCommand = GiveCommand()
+        val ccCommand = CCCommand(giveCommand)
+        
+        getCommand("cc")?.setExecutor(ccCommand)
+        getCommand("cc")?.tabCompleter = giveCommand
+        
         logger.info("CC-Content v${description.version} が有効化されました")
         logger.info("作成者: ${description.authors}")
     }
